b50c516018ea0bff938743e3ba9f6eb02c1d7623
Freewind <nowind_lee@qq.com>
2013-03-21 15:27:33 +0800

    chnage controllers

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,8 @@
 'use strict';
 
 /* Controllers */
-
-
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
-
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+angular.module('myApp.controllers', []).
+    controller('MyCtrl1', ['$scope', function ($scope) {
+    }]).
+    controller('MyCtrl2', ['$scope', function ($scope) {
+    }]);
